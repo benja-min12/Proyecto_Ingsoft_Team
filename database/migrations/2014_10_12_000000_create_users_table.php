ec91e936a2b6126d495364ac4eee59bc33e34794
PabloY2K <pablo.herrera@alumnos.ucn.cl>
2021-10-08 16:35:41 -0300

    primercambio

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('rut')->unique();
+            $table->tinyInteger('status'); //0: deshabilitado; 1: habilitado
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('tipo_usuario')->default(0); // 0 = suscriptor && 1 = administrador
             $table->rememberToken();
             $table->timestamps();
         });
